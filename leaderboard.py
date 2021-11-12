ef8b39ce45276c7fce906c06d00ccf0b918cfafb
DarrRayr <91627952+DarrRayr@users.noreply.github.com>
2021-11-12 11:38:50 -0800

    Update leaderboard.py

--- leaderboard.py
+++ leaderboard.py
@@ -35,6 +35,13 @@ def load_leaderboard(file_name, leader_names, leader_scores):
 			leader_score = leader_score + line[index]
 			index = index + 1
 
+		# the loop ends when the index points at /n
+		print("leader score is:", leader_score)
+
+		# TODO 4: add the player score to the list
+
+		leader_scores.append(int(leader_score))
+
 		
 
 
@@ -42,12 +49,37 @@ def load_leaderboard(file_name, leader_names, leader_scores):
 
 	#update leaderboard by inserting the current player and score to the list at the correct position
 def update_leaderboard(file_name, leader_names, leader_scores, player_name, player_score):
+	leader_index = 0
+	# TODO 5: loop through all the scores in the existing leaderboard list
+	while (leader_index < len(leader_scores)):
+  # TODO 6: check if this is the position to insert new score at
+		if (player_score >= leader_scores[leader_index]):
+			break
+		else:
+			leader_index = leader_index + 1
+	# TODO 7: insert the new player and score at the appropriate position
+		leader_scores.insert(leader_index, player_score)
+		leader_names.insert(leader_index, player_name)
+		print(leader_scores)
+		print(leader_names)
+		
+
+
+  # TODO 8: keep both lists at 5 elements only (top 5 players)
 
 	leader_index = 0
+
+
+
 
 	#store the latest leaderboard back in the file
 	leaderboard_file = open(file_name, "w") #this mode opens the file and erases its contents for a fresh start
 	leader_index = 0
+
+	# TODO 9: loop through all the leaderboard elements and write them to the file
+	while ():
+		leaderboard_file.write(leader_names[leader_index] + "," + str(leader_scores[leader_index]) + "\n")
+		leader_index = leader_index + 1
 
 	leaderboard_file.close()
 
@@ -65,5 +97,26 @@ def draw_leaderboard(leader_names, leader_scores, high_scorer, turtle_object, pl
 	leader_index = 0
 
 	#loop through the lists and use the same index to display the corresponding name and score, seperatred by a tab space '/t'
+	while  leader_index <  len(leader_names):
+		turtle_object.write(str(leader_index + 1) + "/t" + leader_names[leader_index] + "/t" + str(leader_scores[leader_index]), font=font_setup)
+		turtle_object.penup()
+		turtle_object.setposition(-200,int(turtle_object.ycor())-50)
+		turtle_object.pendown()
+		leader_index = leader_index + 1
 
+	#display if user made leaderboard or not
+	if (high_scorer):
+		turtle_object.write("Congratulations! You made the leaderboard!", font=font_setup)
+	else:
+		turtle_object.write("Sorry, you didn't make the leaderboard. Maybe next time!", font=font_setup)
 
+  # move turtle to a new line
+	turtle_object.penup()
+	turtle_object.goto(-200,int(turtle_object.ycor())-50)
+	turtle_object.pendown()
+  
+  # TODO 10: Display a gold/silver/bronze message if player earned a gold/silver/or bronze medal; display nothing if no medal
+	if ():
+		turtle_object.write("You earned a bronze medal!", font=font_setup)
+		turtle_object.write("You earned a silver medal!", font=font_setup)
+		turtle_object.write("You earned a gold medal!", font=font_setup)
